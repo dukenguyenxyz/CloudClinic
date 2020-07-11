4f0dc155ff0e8879a1ff3f61898344bc1aac2a85
Duke <duke.raphael.ng@gmail.com>
2020-07-11 11:34:36 +1000

    Add bcrypt, hash password, remove hashed password from response

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,6 +1,8 @@
 // Instruction: https://www.youtube.com/watch?v=2jqok-WgelI
 
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+
 const User = require('../models/User');
 const { signUpValidation, signInValidation } = require('./validation');
 
@@ -14,17 +16,21 @@ router.post('/signup', async (req, res) => {
   const emailExist = await User.findOne({ email: req.body.email });
   if (emailExist) return res.status(400).send('email already exists');
 
+  // Password hashing
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
   // Create new user from request body
   const user = new User({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password,
+    password: hashedPassword,
   });
 
   // Try to save otherwise send error
   try {
-    const savedUser = await user.save();
-    res.send(savedUser);
+    await user.save();
+    res.send({ user: user._id, email: user.email });
   } catch (err) {
     res.status(400).send(err);
   }
