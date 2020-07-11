1e0838406b0632ca6b0b0eef4db27c478dfbdcd5
Duke <duke.raphael.ng@gmail.com>
2020-07-11 12:11:28 +1000

    Add JWT to package.json and route

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -2,6 +2,7 @@
 
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/User');
 const { signUpValidation, signInValidation } = require('./validation');
@@ -53,7 +54,9 @@ router.post('/signin', async (req, res) => {
   const validPass = await bcrypt.compare(req.body.password, user.password);
   if (!validPass) return res.status(400).send('email or password is incorrect');
 
-  res.send('signed in');
+  // Create and assign a token
+  const token = jwt.sign({ _id: user.id }, process.env.TOKEN_SECRET);
+  res.header('auth-token', token).send(token);
 });
 
 // Sign out
