3b0e4bd041852f2dc5b7c46fb2b84cadd705f68a
Duke <duke.raphael.ng@gmail.com>
2020-07-11 11:05:37 +1000

    Add @hapi/joi and validate userSchema in auth route

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,14 +1,30 @@
+// Instruction: https://www.youtube.com/watch?v=2jqok-WgelI
+// Hapi update: https://github.com/hapijs/joi/issues/2145
+
 const router = require('express').Router();
+const Joi = require('@hapi/joi'); // Validation
 const User = require('../models/User');
+
+const schema = Joi.object({
+  name: Joi.string().min(6).required(),
+  email: Joi.string().min(6).required().email(),
+  password: Joi.string().min(6).required(),
+});
 
 // Sign up
 router.post('/signup', async (req, res) => {
+  // Validation before creation
+  const { error } = schema.validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  // Create new user from request body
   const user = new User({
     name: req.body.name,
     email: req.body.email,
     password: req.body.password,
   });
 
+  // Try to save otherwise send error
   try {
     const savedUser = await user.save();
     res.send(savedUser);
