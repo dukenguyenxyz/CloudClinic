630de3d92a1afed6ad77bd3a134dc6b15b201694
Duke <duke.raphael.ng@gmail.com>
2020-07-15 16:11:03 +1000

    Hash password if it is updated

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const calendarSchema = require('./Calendar');
 
 const userSchema = new mongoose.Schema({
@@ -244,4 +245,17 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-module.exports = mongoose.model('User', userSchema);
+userSchema.pre('save', async function (next) {
+  const user = this;
+
+  if (user.isModified('password')) {
+    const salt = await bcrypt.genSalt(8);
+    user.password = await bcrypt.hash(user.password, salt);
+  }
+
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
