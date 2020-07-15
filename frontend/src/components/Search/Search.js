5905bf6f0a100deb2c8565c971c263688c89e0a3
Harry Buisman <harry@buisman.co>
2020-07-15 14:34:26 +1000

    add search component

---
+++ frontend/src/components/Search/Search.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Search.scss';
+const Search = () => {
+  return (
+    <>
+      <div className="search-wrapper">
+        <input type="text" placeholder="hello worlds"></input>
+      </div>
+    </>
+  );
+};
+
+export default Search;
