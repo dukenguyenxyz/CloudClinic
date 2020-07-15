ce0aede91bd6e9590f6ef5725e098a8bb13d7f49
Duke <duke.raphael.ng@gmail.com>
2020-07-15 20:05:11 +1000

    Verify token and user id belongs to the same model

--- backend/src/routes/verifyToken.js
+++ backend/src/routes/verifyToken.js
@@ -1,11 +1,20 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
   const token = req.header('auth-token');
   if (!token) return res.status(401).send('access denied');
   try {
     const verified = jwt.verify(token, process.env.TOKEN_SECRET);
-    req.user = verified;
+
+    const user = await User.findOne({
+      _id: verified._id,
+      'tokens.token': token,
+    });
+
+    if (!user) throw new Error();
+
+    req.user = user;
     next();
   } catch (err) {
     res.status(400).send('invalid token');
