05e8d654149db1986ed60a53a7677a40f08e90c6
Harry Buisman <harry@buisman.co>
2020-07-16 07:50:14 +1000

    import design system setup files, add axios feathers icons moment and uuid

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import Search from './components/Search/Search';
+import Card from './components/Card/Card';
 
 function App() {
   return (
     <div className="App">
+      <h1 className="bold">Maison Neue 700</h1>
+      <h1 className="medium">Maison Neue 600</h1>
+      <h1 className="demi">Maison Neue 500</h1>
+      <h1 className="book">Maison Neue 400</h1>
+      <h1 className="light">Maison Neue 300</h1>
+      <br />
       <Search />
+      <br />
+      <Card />
     </div>
   );
 }
---
+++ frontend/src/components/Card/Card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Card.scss';
+const Card = () => {
+  return (
+    <div className="card-wrapper">
+      <h1>I'm inside a card</h1>
+    </div>
+  );
+};
+
+export default Card;
