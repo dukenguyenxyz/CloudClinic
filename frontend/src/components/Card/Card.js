29fcb795cc3a8273f5ffd2197d33801a4fd8b76d
Harry Buisman <harry@buisman.co>
2020-07-16 11:28:28 +1000

    fix eslint bugs

--- frontend/src/components/Card/Card.js
+++ frontend/src/components/Card/Card.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './Card.scss';
+
 const Card = () => {
   return (
     <div className="card-wrapper">
       <h1>I'm inside a card</h1>
+      <p>hello</p>
     </div>
   );
 };
