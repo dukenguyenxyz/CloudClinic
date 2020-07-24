5628ec0e18b42c018298c3aa886fd5f09cfa4b84
Duke <duke.raphael.ng@gmail.com>
2020-07-24 10:10:43 +1000

    Revert "Memoize hocs"
    This reverts commit 85a2466bc1ce290dcbd1dc208134dbfa9bb96f53.

--- frontend/src/components/Main/Authentication/Signup/hocs.js
+++ frontend/src/components/Main/Authentication/Signup/hocs.js
@@ -1,8 +1,7 @@
-import React, { memo } from 'react';
+import React from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
-// RawFormWrapper
-const RawFormWrapper = props => {
+export const FormWrapper = props => {
   return (
     <div className="form-wrapper">
       <div className="trim" />
@@ -11,36 +10,20 @@ const RawFormWrapper = props => {
   );
 };
 
-function propsChildrenAreEqual(prevProps, nextProps) {
-  return prevProps.children === nextProps.children;
-}
-
-export const FormWrapper = memo(RawFormWrapper, propsChildrenAreEqual);
-
-// FormHeader
-const RawFormHeader = props => {
+export const FormHeader = ({ title, step }) => {
   return (
     <div classNakme="form-header">
-      <h1>{props.title}</h1>
-      <span>{props.step}</span>
+      <h1>{title}</h1>
+      <span>{step}</span>
     </div>
   );
 };
 
-function propsHeaderAreEqual(prevProps, nextProps) {
-  return (
-    prevProps.title === nextProps.title && prevProps.step === nextProps.step
-  );
-}
-
-export const FormHeader = memo(RawFormHeader, propsHeaderAreEqual);
-
-// ErrorWrapper
-const RawErrorWrapper = props => {
+export const ErrorWrapper = ({ formState }) => {
   return (
     <div className="auth-error-wrapper">
       <ul>
-        {props.formState.errors.map(errorMessage => (
+        {formState.errors.map(errorMessage => (
           <li key={uuidv4()} className="auth-error-message">
             {errorMessage}
           </li>
@@ -49,9 +32,3 @@ const RawErrorWrapper = props => {
     </div>
   );
 };
-
-function propsErrorAreEqual(prevProps, nextProps) {
-  return prevProps.formState.errors === nextProps.formState.errors;
-}
-
-export const ErrorWrapper = memo(RawErrorWrapper, propsErrorAreEqual);
